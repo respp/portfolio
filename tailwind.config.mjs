6386bb21268c575bc0819bbf5ba6bc2a2520c319
Renzo Espinosa B <renzoespinosa.b@gmail.com>
2024-02-26 01:45:43 -0300

    modified proyect cards

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,16 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	darkMode: 'class',
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				deepBlue: "#0E1023",
+				lightBlue: "#CAF9FF",
+						light1: "#263A9C",
+						light2: "#15FACE",
+						light3: "#6223C1",
+						patito: "#A0D7FF",
+			  },
+		},
 	},
 	plugins: [],
 }
