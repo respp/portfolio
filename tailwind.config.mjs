669d21a5b63fb029609550b3b7216e29a11c25ab
Renzo Espinosa B <renzoespinosa.b@gmail.com>
2024-03-03 01:26:02 -0300

    all changes

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -14,7 +14,11 @@ export default {
 						less:"#3448AF56",
 						light2: "#15FACE",
 						light3: "#6223C1",
+						titles: '#5FACD7',
 			  }, 
+			  linearGradients: {
+				borderColor: 'linear-gradient(100deg, rgb(255, 127, 251), rgb(122,200, 255))',
+			  },
 		},
 	},
 	plugins: [require('tailwind-scrollbar')],
